e7d5340d9e6236a7941371e1bc4789c98ee72404
andy1138 <andy@hicks.net>
2015-07-19 14:00:12 +0100

    Basic list of scala meetups

--- build.sbt
+++ build.sbt
@@ -4,11 +4,23 @@ version := "1.0"
 
 scalaVersion := "2.10.5"
 
-libraryDependencies ++= Seq("com.propensive" %% "rapture-core" % "0.9.0")
+//libraryDependencies ++= Seq("com.propensive" %% "rapture-core" % "0.9.0")
 
-libraryDependencies ++= Seq("com.propensive" %% "rapture-json" % "0.9.0")
+//libraryDependencies ++= Seq("com.propensive" %% "rapture-json" % "0.9.0")
 
 //libraryDependencies ++= Seq("com.propensive" %% "rapture-io" % "0.9.0")
 
-libraryDependencies ++= Seq("com.propensive" %% "rapture-net" % "0.10.0")
+//libraryDependencies ++= Seq("com.propensive" %% "rapture-net" % "0.10.0")
+
+
+libraryDependencies ++= Seq(
+  "org.specs2" %% "specs2" % "2.3.12" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.0" % "test",
+  "com.propensive" %% "rapture-io" % "0.9.1" withSources() withJavadoc(),
+  "com.propensive" %% "rapture-json" % "0.9.1" withSources() withJavadoc(),
+  "com.propensive" %% "rapture-json-jackson" % "0.9.0" withSources() withJavadoc(),
+  "com.propensive" %% "rapture-core" % "0.9.0" withSources() withJavadoc(),
+  "com.propensive" %% "rapture-net" % "0.9.0" withSources() withJavadoc(),
+  "com.propensive" %% "rapture-uri" % "0.9.1" withSources() withJavadoc()
+)
 
--- src/main/scala/lsug/scalaspace/Main.scala
+++ src/main/scala/lsug/scalaspace/Main.scala
@@ -3,30 +3,42 @@ package lsug.scalaspace
 
 
 import rapture._
-import core._, io._, net._, uri._, json._, codec._
+// import core._, io._, net._, uri._, json._, codec._
 
-import rapture.json.jsonBackends.json._
+import rapture.io._
+import rapture.core._
+import rapture.json._
+import rapture.uri._
+import rapture.net._
+import rapture.core.strategy.throwExceptions
 
 
-// Read a file into a string
-import encodings.`UTF-8`
-
-// Parse it as Json
-//import jsonBackends.jackson._
-
+import jsonParsers.scalaJson._
 
 
 object Main extends App {
-  println("Hello")
+   implicit val enc = Encodings.`UTF-8`
 
-  val src = uri"https://api.meetup.com/find/groups?page=20&text=scala&key=2393d8e367439537d751513a6a12&sign=true&format=json".slurp[Char]
 
-  val json = Json.parse(src)
-  println(json)
+  def loadPage(offsetPage:Int) = {
 
-  // Auto-extract a `Group` into a case class structure
-//  case class Member(name: String, born: Int)
-//  case class Group(groupName: String, members: Set[Member])
-//  json.groups(0).as[Group]
+    val src = uri"https://api.meetup.com/find/groups?page=1000&radius=global&text=scala&key=2393d8e367439537d751513a6a12&format=json".slurp[Char]
+
+    val json = Json.parse(src).as[List[Json]].filter( _.name.toString.toLowerCase.contains("scala"))
+
+    val names = json.map( l => (l.name, l.link, l.lat, l.lon ) )
+
+    names.map( l => ( s"  [${l._1},${l._2},${l._3},${l._4},true]" ) )
+  }
+
+  println("var groups = [")
+  val x = loadPage(1)
+  println(x.sortBy( l => l.toLowerCase).mkString(",\n"))
+
+  println("];")
+
+
+  println(s"Count ${x.length}")
+
 
 }
